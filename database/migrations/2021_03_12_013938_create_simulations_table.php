c4543e7dcd21c46b5f1d475c32d80cea3ec4f960
RoyStorey <imlejiii@gmail.com>
2021-03-11 20:59:59 -0500

    comonman

---
+++ database/migrations/2021_03_12_013938_create_simulations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSimulationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('simulations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+
+            $table->integer('score');
+            $table->string('created_origin');
+            $table->integer('inject_score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('simulations');
+    }
+}
